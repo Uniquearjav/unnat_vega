326dd47ccf9057542f7e9181df09fabbef084045
Arjav Choudhary <arjavchoudhary608@gmail.com>
2026-04-05 21:00:29 +0530

    feat: add Google Analytics component for enhanced tracking

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
         <Navbar />
         <main>{children}</main>
         <Footer />
+        <GoogleAnalytics gaId="G-4D4EXH14R3" />
       </body>
     </html>
   );
